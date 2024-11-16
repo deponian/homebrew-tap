eaf440a73fa787f8d436e351a5bdeb0d98d88370
Rufus Deponian <31796184+deponian@users.noreply.github.com>
2024-11-16 19:15:55 +0000

    logalize: add 0.4.1 bottle.

--- Formula/logalize.rb
+++ Formula/logalize.rb
@@ -7,6 +7,13 @@ class Logalize < Formula
   license "MIT"
   head "https://github.com/deponian/logalize.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/deponian/homebrew-tap/releases/download/logalize-0.4.1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "33f77f4260aad1c9eb4c3955ff56a8671254cc210cad0367d8d5bf4395b774b2"
+    sha256 cellar: :any_skip_relocation, ventura:       "46991b960b4ab54c0dc0ca6389bc1643dfa56df0013687c626aebc6c5ce02b74"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b84f504f9673eca4c0f1aefb1e16aeae2a6c78d979feef72036694e4eba4b40b"
+  end
+
   depends_on "go" => :build
 
   def install
